ae3b16fb489d3e81b1f82fa3de350d51d5986aba
John DiMatteo <jdimatte@uwaterloo.ca>
2018-09-11 18:56:35 -0400

    changed function to a class and added predict

--- python_/perceptron/perceptron.py
+++ python_/perceptron/perceptron.py
@@ -1,5 +1,7 @@
 '''
 Perceptron Algorithm developed in 1958 by Rosenblatt
+ex.
+python3 perceptron data.txt "1,1,0,1,1"
 '''
 import numpy as np
 import sys
@@ -10,23 +12,35 @@ def fetch_data(filename):
     data = genfromtxt(filename, delimiter=',', skip_header=True)
     return data
 
-def Perceptron(D):
-    w = np.zeros(np.size(D,1)-1)
-    b = 0
-    threshold = 0
-    while True:
-        for instance in D:
-            x = instance[:-1]
-            y = instance[-1]
-            if y*(np.dot(w,x) + b) <= threshold:
-                w += y*x
-                b += y
-            print("Weight Vector:", w)
-            time.sleep(1)
+class Perceptron:
+    def __init__(self, D):
+        self.w = np.zeros(np.size(D,1)-1)
+        self.b = 0
+        self.threshold = 0
+
+    def run(self,D,iter):
+        for i in range(iter):
+            for instance in D:
+                x = instance[:-1]
+                y = instance[-1]
+                if y*(np.dot(self.w,x) + self.b) <= self.threshold:
+                    self.w += y*x
+                    self.b += y
+                print("Weight Vector:", self.w)
+                time.sleep(1)
+
+    def predict(self, x):
+        return np.dot(self.w,x) + self.b
 
 filename = sys.argv[1]
+new_data = np.asarray(sys.argv[2].split(','), dtype = float)
+print(new_data)
 D = fetch_data(filename)
 if np.size(D) != 0:
-    Perceptron(D)
+    P = Perceptron(D)
+    P.run(D, 1)
+    y = P.predict(new_data)
+    print("Prediction:", y)
+
 else:
     print("Data empty or wrong filename")
